b649546769def9ded6c768701add1a06886ec970
Kate Dameron <katerj33@gmail.com>
2019-03-16 13:14:04 -0700

    logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <figure>
+      <img src="../assets/logo"
+        alt="Logo for Kate Dameron - black and white abstracted shapes representing the intials KRD"
+      />
+    </figure>
+  );
+}
