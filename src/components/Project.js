c0a524069cfe438c15fa741659cefdde9cde08ab
Kate Dameron <katerj33@gmail.com>
2019-03-16 17:05:22 -0700

    Project component renders a project using the figure and projectDetails components

---
+++ src/components/Project.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProjectDetails from './ProjectDetails';
+import Figure from './Figure';
+
+function Project({ project }) {
+  const { imageUrl, alt, title, media, about, siteURL, githubURL } = project;
+  return (
+    <section>
+      <Figure 
+        imageUrl={imageUrl}
+        alt={alt}
+      />
+      <ProjectDetails 
+        title={title}
+        media={media}
+        about={about}
+        siteURL={siteURL}
+        githubURL={githubURL}
+      />
+    </section>
+  );
+}
+
+Project.propTypes = {
+  project: PropTypes.object.isRequired
+};
