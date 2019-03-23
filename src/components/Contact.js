4ecec8d09bfe8e1d9ab9cb7d4e5a0bcc88b244b1
Kate Dameron <katerj33@gmail.com>
2019-03-23 11:38:00 -0700

    conact layout, home mobile layout, project fade

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import styles from '../styles/contact.css';
 export default function Contact() {
   return (
     <ul className={styles.ul}>
-      <li><A href='#'>hello@katedameron.com</A></li>
+      <li><A href='href="mailto:hello@katedameron.com"'>hello@katedameron.com</A></li>
       <li><A href='https://www.linkedin.com/in/kate-dameron/'><img src="../../assets/color/iconfinder_linkedin_252090.png" alt="LinkedIn icon"/></A></li>
       <li><A href='https://github.com/Katedam'><img src="../../assets/color/iconfinder_github2_252065.png" alt="Github icon"/></A></li>
       <li><A href='https://twitter.com/katerj'><img src="../../assets/color/iconfinder_f-twitter_256_282459.png" alt="Twitter icon"/></A></li>
