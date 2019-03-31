a1af4efee73232ca775a9d04d1c5cf56639d9806
Kate Dameron <katerj33@gmail.com>
2019-03-31 10:15:14 -0700

    about page updates

--- src/components/About.js
+++ src/components/About.js
@@ -10,22 +10,17 @@ export default function About() {
 
       <section className={styles.section}>
         <article>
-          <h2>About</h2>
-          <p>Fullstack Software Developer with a background in arts education in the non-profit sector seeking a role with a team where I may utilize my technical knowledge in combination with my creative skill-set to build innovative and dynamic applications that improve our community</p>
+          <h2>bio</h2>
+          <p>As a Full-stack Developer with a background in arts education I enjoy making accessible tools that improve our community. When I am not building components and working on my debugging superpowers - I enjoys hiking with my philosophical pup and husband, baking and making art.</p>
         </article>
       </section>
     
       <section className={styles.section}>
         <article>
-          <h2>Stengths</h2>
-          <p>I have a strong understanding of the MERN stack. I enjoy working on a good puzzle and am always working on my debugging superpowers. I am highly motivated by goals and dedicated to improvement and growth with the onset of each new task. My adaptability and empathy make me a strong collaborator who values harmony when working towards a shared mission and vision</p>
+          <h2>highlights</h2>
+          <p>I have a strong understanding of the MERN stack. I enjoy dissecting a good puzzle and working on my debugging superpowers. I am highly motivated by goals and dedicated to improvement and growth with the onset of each new task. My adaptability and empathy make me a strong collaborator who values harmony when working towards a shared mission and vision. I find mob and pair programming to be highly valuable both educationally and collaboratively.</p>
         </article>
       </section>
-
-      {/* <section className={styles.section}>
-        <h2>Something else...</h2>
-        <p>Fullstack Software Developer with a background in arts education in the non-profit sector seeking a role with a team where I may utilize my technical knowledge in combination with my createive skill-set to build innovative and dynamic applications that improve our community</p>
-      </section> */}
     </main>
   );
 }
