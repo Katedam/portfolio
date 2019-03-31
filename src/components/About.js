ddce706935a784435eeb1a2a8924289357217717
Kate Dameron <katerj33@gmail.com>
2019-03-31 16:31:27 -0700

    added triangle to about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,19 +5,20 @@ export default function About() {
   return (
     <main className={styles.wrapper}>
       <header className={styles.header}>
-        <h1>about</h1>
+        <h1>About</h1>
+        <img src="https://res.cloudinary.com/dzxqhkyqd/image/upload/a_270/v1553999875/triangle_copy.png"/>
       </header>
 
       <section className={styles.section}>
         <article>
-          <h2>bio</h2>
+          <h2>Bio</h2>
           <p>As a Full-stack Developer with a background in arts education I enjoy making accessible tools that improve our community. When I am not building components and working on my debugging superpowers - I like hiking with my philosophical pup and husband, baking and making art.</p>
         </article>
       </section>
     
       <section className={styles.section}>
         <article>
-          <h2>highlights</h2>
+          <h2>Highlights</h2>
           <p>I have a strong understanding of the MERN stack. I enjoy dissecting a good puzzle and working on my debugging superpowers. I am highly motivated by goals and dedicated to improvement and growth with the onset of each new task. My adaptability and empathy make me a strong collaborator who values harmony when working towards a shared mission and vision. I find mob and pair programming to be highly valuable both educationally and collaboratively.</p>
         </article>
       </section>
