06e0706ba16bf9d84c1366affb61074cf3120453
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-02-02 16:53:57 -0800

    refactor footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import styles from '../styles/footer.css';
+import styled from 'styled-components';
 
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <h6>Made by Kate R. Dameron &copy;</h6>
-    </footer>
-  );
-}
+const Wrap = styled.footer`
+  background-color: whitesmoke;
+  color: black;
+  position: relative;
+  bottom: 0;
+  width: 100vw;
+  text-align: center;
+  margin-top: 0;
+  font-family: sans-serif;
+  box-shadow: inset 5px 0 5px -2px #e0dfdf;
+
+  @media (min-width: 760px) {
+    width: 75vw;
+    left: 25vw;
+  }
+`;
+
+const Heading6 = styled.h6`margin: 10px;`;
+
+const Footer = () => (
+  <Wrap>
+    <Heading6>Made by Kate R. Dameron &copy; 2019</Heading6>
+  </Wrap>
+);
+
+export default Footer
+
